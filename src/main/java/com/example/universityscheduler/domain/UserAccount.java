f30afa8a14143b0dfe95916293a35f9f5a4e2bb5
amiroxumbayev <amiroxumbayev@gmail.com>
2023-03-26 12:03:08 +0600

    Added user account

---
+++ src/main/java/com/example/universityscheduler/domain/UserAccount.java
@@ -0,0 +1,45 @@
+package com.example.universityscheduler.domain;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.Hibernate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import java.util.Objects;
+import java.util.UUID;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Entity
+public class UserAccount {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    private String email;
+    private String password;
+    @OneToOne(mappedBy = "userAccount")
+    @ToString.Exclude
+    private University university;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        UserAccount that = (UserAccount) o;
+        return id != null && Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, email);
+    }
+}
