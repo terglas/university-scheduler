a29e7a6b6c28d91cc0d2de0753277663760d9f22
amiroxumbayev <amiroxumbayev@gmail.com>
2023-04-16 19:04:24 +0600

    added endpoints find and create group

---
+++ src/main/java/com/example/universityscheduler/controller/GroupController.java
@@ -0,0 +1,12 @@
+package com.example.universityscheduler.controller;
+
+import com.example.universityscheduler.api.GroupzApi;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class GroupController implements GroupzApi {
+
+
+}
