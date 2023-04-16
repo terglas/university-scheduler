e54dcf14a9b66262613b3a97a4a16ef5a00321c6
amiroxumbayev <amiroxumbayev@gmail.com>
2023-04-16 23:13:21 +0600

    added endpoints for schedules

---
+++ src/main/java/com/example/universityscheduler/controller/ScheduleController.java
@@ -0,0 +1,10 @@
+package com.example.universityscheduler.controller;
+
+import com.example.universityscheduler.api.SchedulezApi;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ScheduleController implements SchedulezApi {
+}
