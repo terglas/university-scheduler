78dab11a5be007ca71978345edf4528fffe1bfdb
amiroxumbayev <amiroxumbayev@gmail.com>
2023-04-23 22:53:08 +0600

    implemented service for find by query

--- src/main/java/com/example/universityscheduler/service/impl/ScheduleServiceImpl.java
+++ src/main/java/com/example/universityscheduler/service/impl/ScheduleServiceImpl.java
@@ -4,15 +4,19 @@ import com.example.universityscheduler.domain.Schedule;
 import com.example.universityscheduler.exception.NotFoundException;
 import com.example.universityscheduler.mapper.ScheduleMapper;
 import com.example.universityscheduler.model.PageParams;
+import com.example.universityscheduler.model.SearchQuery;
+import com.example.universityscheduler.model.SearchType;
 import com.example.universityscheduler.repository.ScheduleRepository;
 import com.example.universityscheduler.service.ScheduleService;
 import lombok.RequiredArgsConstructor;
 import lombok.val;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 
@@ -30,8 +34,12 @@ public class ScheduleServiceImpl implements ScheduleService {
     }
 
     @Override
-    public List<Schedule> findAll(PageParams pageParams) {
+    public List<Schedule> findAll(Optional<SearchQuery> query, PageParams pageParams) {
         val pageable = PageRequest.of(pageParams.getPageCurrent() - 1, pageParams.getPageSize());
+        if(query.isPresent()) {
+            val searchQuery = query.get();
+            return getSchedulesByFilter(searchQuery.getSearchType(), searchQuery.getSearchId(), pageable);
+        }
         return scheduleRepository.findAll(pageable).getContent();
     }
 
@@ -54,5 +62,25 @@ public class ScheduleServiceImpl implements ScheduleService {
         val schedule = scheduleRepository.findById(id)
                 .orElseThrow(() -> new NotFoundException(String.format("Schedule not found: %S", id)));
         scheduleRepository.delete(schedule);
+    }
+
+
+    private List<Schedule> getSchedulesByFilter(SearchType type, UUID id, Pageable pageable) {
+        switch (type) {
+            case TEACHER:
+                return scheduleRepository.findByTeacherId(id, pageable).getContent();
+            case GROUP:
+                return scheduleRepository.findByGroupId(id, pageable).getContent();
+            case SUBJECT:
+                return scheduleRepository.findBySubjectId(id, pageable).getContent();
+            /* TODO
+            case UNIVERSITY:
+                return scheduleRepository.findByUniversityId(id, pageable).getContent();
+            case EDUCATIONAL_PROGRAM:
+                return scheduleRepository.findByEducationalProgramId(id, pageable).getContent();
+            */
+            default:
+                throw new IllegalArgumentException("Unexpected value: " + type);
+        }
     }
 }
