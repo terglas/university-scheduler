75e36fb62ba0993a75587d1799ef88b8eb08320c
amiroxumbayev <amiroxumbayev@gmail.com>
2023-04-23 22:52:49 +0600

    implemented query by group

--- src/main/java/com/example/universityscheduler/repository/ScheduleRepository.java
+++ src/main/java/com/example/universityscheduler/repository/ScheduleRepository.java
@@ -1,9 +1,20 @@
 package com.example.universityscheduler.repository;
 
 import com.example.universityscheduler.domain.Schedule;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.UUID;
 
-public interface ScheduleRepository  extends JpaRepository<Schedule, UUID> {
+public interface ScheduleRepository extends JpaRepository<Schedule, UUID> {
+
+    @Query(name = "Schedule.findByGroupId", nativeQuery = true)
+    Page<Schedule> findByGroupId( @Param("groupId") UUID groupId, Pageable pageable);
+
+    Page<Schedule> findByTeacherId(UUID teacherId, Pageable pageable);
+
+    Page<Schedule> findBySubjectId(UUID subjectId, Pageable pageable);
 }
