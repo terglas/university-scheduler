018e7def62914ae620bc68ef57534fb98a890a51
amiroxumbayev <amiroxumbayev@gmail.com>
2023-05-13 19:34:41 +0600

    fix auth mapper

--- src/main/java/com/example/universityscheduler/mapper/AuthMapper.java
+++ src/main/java/com/example/universityscheduler/mapper/AuthMapper.java
@@ -2,70 +2,25 @@ package com.example.universityscheduler.mapper;
 
 import com.example.universityscheduler.domain.UserAccount;
 import com.example.universityscheduler.model.AuthRequest;
-import org.keycloak.representations.UserInfo;
-import org.keycloak.representations.idm.CredentialRepresentation;
-import org.keycloak.representations.idm.UserRepresentation;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.factory.Mappers;
-
-import java.util.List;
+import org.mapstruct.Named;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Mapper
-public interface AuthMapper {
+public abstract class AuthMapper {
+    @Autowired
+    protected PasswordEncoder passwordEncoder;
 
-    AuthMapper INSTANCE = Mappers.getMapper(AuthMapper.class);
+    @Mapping(target = "password", source = "password", qualifiedByName = "encode")
+    public abstract UserAccount map(AuthRequest src);
 
-    UserAccount map(AuthRequest src);
+    public abstract AuthRequest toLoginRequest(String email, String password);
 
-    AuthRequest toLoginRequest(String email, String password);
-
-    UserInfo mapUserInfo(UserAccount src);
-
-    @Mapping(target = "username", source = "email")
-    @Mapping(target = "firstName", ignore = true)
-    @Mapping(target = "lastName", ignore = true)
-    @Mapping(target = "enabled", constant = "true")
-    @Mapping(target = "groups", ignore = true)
-    @Mapping(target = "self", ignore = true)
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "createdTimestamp", ignore = true)
-    @Mapping(target = "totp", ignore = true)
-    @Mapping(target = "emailVerified", ignore = true)
-    @Mapping(target = "attributes", ignore = true)
-    @Mapping(target = "credentials", ignore = true)
-    @Mapping(target = "requiredActions", ignore = true)
-    @Mapping(target = "federatedIdentities", ignore = true)
-    @Mapping(target = "socialLinks", ignore = true)
-    @Mapping(target = "realmRoles", ignore = true)
-    @Mapping(target = "clientRoles", ignore = true)
-    @Mapping(target = "clientConsents", ignore = true)
-    @Mapping(target = "notBefore", ignore = true)
-    @Mapping(target = "federationLink", ignore = true)
-    @Mapping(target = "serviceAccountClientId", ignore = true)
-    @Mapping(target = "origin", ignore = true)
-    @Mapping(target = "disableableCredentialTypes", ignore = true)
-    @Mapping(target = "access", ignore = true)
-    @Mapping(target = "applicationRoles", ignore = true)
-    UserRepresentation toUserRepresentation(AuthRequest src);
-
-    default UserRepresentation mapUserRepresentation(AuthRequest src) {
-        final var tgt = toUserRepresentation(src);
-        final var cred = mapPasswordCredentials(src.getPassword());
-
-        tgt.setCredentials(List.of(cred));
-        return tgt;
+    @Named("encode")
+    protected String encode(String password) {
+        return passwordEncoder.encode(password);
     }
 
-    @Mapping(target = "value", source = "password")
-    @Mapping(target = "temporary", expression = "java(Boolean.FALSE)")
-    @Mapping(target = "type", expression = "java(CredentialRepresentation.PASSWORD)")
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "userLabel", ignore = true)
-    @Mapping(target = "secretData", ignore = true)
-    @Mapping(target = "credentialData", ignore = true)
-    @Mapping(target = "priority", ignore = true)
-    @Mapping(target = "createdDate", ignore = true)
-    @Mapping(target = "config", ignore = true)
-    CredentialRepresentation mapPasswordCredentials(String password);
 }
