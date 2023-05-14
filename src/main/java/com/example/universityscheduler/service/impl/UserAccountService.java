c41e8125777e7b1633285da418585c7818a4cfa3
amiroxumbayev <amiroxumbayev@gmail.com>
2023-05-14 17:23:18 +0600

    implemented user service

--- src/main/java/com/example/universityscheduler/service/impl/UserAccountService.java
+++ src/main/java/com/example/universityscheduler/service/impl/UserAccountService.java
@@ -1,55 +1,55 @@
-//package com.example.universityscheduler.service.impl;
-//
-//import com.example.universityscheduler.domain.UserAccount;
-//import com.example.universityscheduler.exception.BadAuthorizeException;
-//import com.example.universityscheduler.exception.ForbiddenException;
-//import com.example.universityscheduler.exception.NotFoundException;
-//import com.example.universityscheduler.repository.UserAccountRepository;
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.security.core.context.SecurityContextHolder;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.Optional;
-//
-//@Service
-//@Slf4j
-//@RequiredArgsConstructor
-//public class UserAccountService {
-//    private final UserAccountRepository userRepo;
-//
-//    public UserAccount getCurrentUserAccount() {
-//        final var username = getUserAccountName();
-//        return Optional.of(username)
-//                .map(userRepo::findByEmail)
-//                .orElseThrow(() -> {
-//                    log.warn("Not found user {} in the service", username);
-//                    return new ForbiddenException();
-//                });
-//    }
-//
-//    public String getUserAccountName() {
-//       final var token = getToken();
-//        return Optional.of(token)
-//                .map(AccessToken::getPreferredUsername)
-//                .orElseThrow(() -> {
-//                    log.warn("Error token is bad preferred_username token_id={}, subject={}",
-//                            token.getId(), token.getSubject());
-//                    return new BadAuthorizeException();
-//                });
-//    }
-//
-//    public AccessToken getToken() {
-//        final var context = SecurityContextHolder.getContext().getAuthentication();
-//        return Optional.ofNullable(context)
-//                .filter(KeycloakAuthenticationToken.class::isInstance)
-//                .map(KeycloakAuthenticationToken.class::cast)
-//                .map(KeycloakAuthenticationToken::getAccount)
-//                .map(OidcKeycloakAccount::getKeycloakSecurityContext)
-//                .map(KeycloakSecurityContext::getToken)
-//                .orElseThrow(BadAuthorizeException::new);
-//    }
-//
+package com.example.universityscheduler.service.impl;
+
+import com.example.universityscheduler.domain.UserAccount;
+import com.example.universityscheduler.exception.BadAuthorizeException;
+import com.example.universityscheduler.exception.ForbiddenException;
+import com.example.universityscheduler.exception.NotFoundException;
+import com.example.universityscheduler.repository.UserAccountRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class UserAccountService {
+    private final UserAccountRepository userRepo;
+
+    public UserAccount getCurrentUser() {
+        final var username = getUserAccountName();
+        return Optional.of(username)
+                .map(userRepo::findByEmail)
+                .orElseThrow(() -> {
+                    log.warn("Not found user {} in the service", username);
+                    return new ForbiddenException();
+                });
+    }
+
+    public String getUserAccountName() {
+       final var token = getToken();
+        return Optional.of(token)
+                .map(UserDetails::getUsername)
+                .orElseThrow(() -> {
+                    log.warn("Not found user in the token");
+                    return new BadAuthorizeException("Not found user in the token");
+                });
+    }
+
+    public UserDetails getToken() {
+        final var context = SecurityContextHolder.getContext().getAuthentication();
+        return Optional.ofNullable(context)
+                .filter(authentication -> authentication.getPrincipal() instanceof UserDetails)
+                .map(authentication -> (UserDetails) authentication.getPrincipal())
+                .orElseThrow(() -> {
+                    log.warn("Not found user in the token");
+                    return new ForbiddenException("Not authorized");
+                });
+    }
+
 //    public AccessToken getToken(String token) {
 //        try {
 //            return TokenVerifier.create(token, AccessToken.class).getToken();
@@ -58,13 +58,13 @@
 //            return null;
 //        }
 //    }
-//
-//    public UserAccount findByEmail(String email) {
-//        UserAccount
-//                user = userRepo.findByEmail(email);
-//        if (user != null) {
-//            return user;
-//        }
-//        throw new NotFoundException("UserAccount " + email + " not found");
-//    }
-//}
+
+    public UserAccount findByEmail(String email) {
+        UserAccount
+                user = userRepo.findByEmail(email);
+        if (user != null) {
+            return user;
+        }
+        throw new NotFoundException("UserAccount " + email + " not found");
+    }
+}
