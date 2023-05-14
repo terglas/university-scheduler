0df9ea32eab3da252aa36da9246d9cad636401b7
amiroxumbayev <amiroxumbayev@gmail.com>
2023-05-14 18:57:55 +0600

    added university id for subject dto

--- src/main/java/com/example/universityscheduler/mapper/rest/SubjectRestMapper.java
+++ src/main/java/com/example/universityscheduler/mapper/rest/SubjectRestMapper.java
@@ -10,9 +10,11 @@ import java.util.UUID;
 
 @Mapper
 public interface SubjectRestMapper {
-
+    @Mapping(target = "university.id", source = "universityId")
     Subject toEntity(SubjectExtendedInfo subjectDTO);
     Subject toEntity(SubjectShortInfo subjectDTO);
+
+    @Mapping(target = "universityId", source = "university.id")
     SubjectExtendedInfo toExtendedDto(Subject subject);
     SubjectShortInfo toShortDto(Subject subject);
 
