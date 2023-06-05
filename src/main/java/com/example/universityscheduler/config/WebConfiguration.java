5c9eb0c20e0e31b600cd169306b100057f94eb60
amiroxumbayev <amiroxumbayev@gmail.com>
2023-06-05 20:42:23 +0600

    enabling scheduler

--- src/main/java/com/example/universityscheduler/config/WebConfiguration.java
+++ src/main/java/com/example/universityscheduler/config/WebConfiguration.java
@@ -1,10 +1,12 @@
 package com.example.universityscheduler.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableScheduling
 public class WebConfiguration implements WebMvcConfigurer {
 
     @Override
